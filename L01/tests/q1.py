45642ac2ba56b120149fc2299d2596143819bbdd
your-username <you@example.com>
2026-09-13 07:40:03 +0000

    L01 rebuild

--- L01/tests/q1.py
+++ L01/tests/q1.py
@@ -2,4 +2,11 @@ OK_FORMAT = True
 
 test = {   'name': 'q1',
     'points': 10,
-    'suites': [{'cases': [{'code': '>>> assert mean_of([1, 2, 3]) == 2\n', 'hidden': False, 'locked': False, 'points': 4}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [   {   'cases': [   {   'code': ">>> def test_q1(mean_of, assert_equal):\n...     assert_equal('mean_of([1, 2, 3])', mean_of([1, 2, 3]), 2)\n>>> test_q1(mean_of, assert_equal)\n",
+                                       'hidden': False,
+                                       'locked': False,
+                                       'points': 4}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
