44c0806a61b1dc1bcde52e064eea2b5dd3b03beb
your-username <you@example.com>
2026-09-13 07:52:09 +0000

    L01 rebuild

--- L01/tests/q1.py
+++ L01/tests/q1.py
@@ -7,5 +7,6 @@ points = 10
 
 @test_case(points=4, hidden=False)
 def test_q1(mean_of, assert_equal):
-    assert_equal('mean_of([1, 2, 3])', mean_of([1, 2, 3]), 2)
+    y = mean_of([1, 2, 3])
+    assert_equal('y', y, 2)
 
