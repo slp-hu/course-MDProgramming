65e73431428bcf5de4082834960452059198cb18
slp-hu <itou@hosei.ac.jp>
2026-09-13 06:57:26 +0000

    L01

---
+++ L01/tests/q1.py
@@ -0,0 +1,5 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1',
+    'points': 10,
+    'suites': [{'cases': [{'code': '>>> assert mean_of([1, 2, 3]) == 2\n', 'hidden': False, 'locked': False, 'points': 4}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
